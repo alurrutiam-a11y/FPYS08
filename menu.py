e28974d74918c396d8cefacea4817b81b9eda9d2
VI-Alumno <VI-Alumno@duoc.acad>
2026-06-26 14:47:59 -0400

    archivo inicial

--- menu.py
+++ menu.py
@@ -5,4 +5,20 @@ def menu():
     print("3.saludar")
     print("4.salir")
     
+    opcion = int(input("ingrese una opcion:"))
+    if opcion == 1:
+        print(">>> sumando")
+
+    elif opcion == 2:
+        print(">>> restando")
+
+    elif opcion == 3:
+        print(">>> saludando")
+
+    elif opcion == 4:
+        print(">>> saliendo")
+        break
+    else:
+        print("opcion invalida (1,4)")
+
 menu()
